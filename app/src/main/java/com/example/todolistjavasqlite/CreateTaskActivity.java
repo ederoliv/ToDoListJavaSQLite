820c17f529bf5f99e2f1f227800278224f3e4cc2
ederoliv <ederoliveira152@gmail.com>
2023-08-16 00:23:10 -0300

    Commit

--- app/src/main/java/com/example/todolistjavasqlite/CreateTaskActivity.java
+++ app/src/main/java/com/example/todolistjavasqlite/CreateTaskActivity.java
@@ -2,13 +2,25 @@ package com.example.todolistjavasqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class CreateTaskActivity extends AppCompatActivity {
+
+    EditText editTextName;
+    Button button;
+    SQLiteDatabase database;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_edita);
+        setContentView(R.layout.activity_create);
+
+        editTextName = (EditText) findViewById(R.id.editTextName);
+        button = (Button) findViewById(R.id.buttonNewTask);
+
+
     }
 }
--- app/src/main/java/com/example/todolistjavasqlite/MainActivity.java
+++ app/src/main/java/com/example/todolistjavasqlite/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 
+
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
